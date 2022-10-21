c90f47882b42385ed9bcf0a6a0a42a8b2365f955
Malva2312 <up202006605@edu.fe.up.pt>
2022-10-22 00:46:11 +0100

    PFL normalização, soma, multiplicação

---
+++ poli.hs
@@ -0,0 +1,95 @@
+import Data.List
+
+
+data Var = Var { 
+    var :: String, 
+    expoent :: Integer
+} deriving (Eq,Ord, Show)
+
+data Mono = Mono { 
+    vars :: [Var],
+    coef :: Integer
+} deriving (Eq,Ord, Show)
+
+-- let x = Mono (sort (vars x)) (coef x)
+orderMono :: Mono -> Mono
+orderMono x =  Mono (sort (vars x)) (coef x)
+
+equalVars :: Mono -> Mono -> Bool
+equalVars x y = (vars x == vars y)
+
+notEqualVars :: Mono -> Mono -> Bool
+notEqualVars x y = not (equalVars x y)
+
+normalize :: [Mono] -> [Mono]
+normalize [] = []
+normalize (x:xs) = [Mono (vars x) new_coef] ++ (normalize [y | y <- xs, (vars x /= vars y)])
+    where new_coef = sum (map coef [y | y <- (x:xs), (vars x == vars y)] ) --  [coef y | y <- (x:xs), (vars x == vars y)] )   
+
+norm :: [Mono] -> [Mono]
+norm [] = []
+norm ms = normalize (sort (map orderMono ms))
+
+addiction :: [Mono] -> [Mono] -> [Mono]
+addiction x y = norm (x ++ y)
+
+negMultiply :: Mono -> Mono
+negMultiply x = Mono (vars x) ((coef x) * (-1))
+
+subtraction :: [Mono] -> [Mono] -> [Mono]
+subtraction x y = norm (x ++ (map negMultiply y))
+
+clean :: [Mono] -> [Mono]
+clean [] = []
+clean x = [y | y <- x, ((coef y) /= 0 )]
+
+addVars :: [Var] -> [Var] -> [Var]
+addVars [] [] = []
+addVars [] y = y
+addVars x [] = x
+addVars (x:xs) (y:ys) = [Var (var x) (sum (map expoent ([z | z <- (x:xs), (var z == var x)] ++ [z | z <- (y:ys), (var z == var x)]))) ] ++ (addVars [z | z <- (x:xs), (var z /= var x)] [z | z <- (y:ys), (var z /= var x)])
+
+
+multiplyMono :: Mono -> Mono -> Mono
+multiplyMono x y = Mono (addVars (vars x) (vars y)) ((coef x) * (coef y) )
+
+
+multiply :: Mono -> [Mono] -> [Mono]
+multiply x [] = []
+multiply x (y:ys) 
+    | (coef x == 0) = [] ++ (multiply x ys)
+    | otherwise = [multiplyMono x y] ++ (multiply x ys)
+
+
+multiplyPoly :: [Mono] -> [Mono] -> [Mono]
+multiplyPoly [] [] = []
+multiplyPoly [] y  = []
+multiplyPoly x  [] = []
+multiplyPoly (x:xs) y = (multiply x y) ++ (multiplyPoly xs y)
+
+mult :: [Mono] -> [Mono] -> [Mono]
+mult [] [] = []
+mult [] y  = []
+mult x  [] = []
+mult x  y  = normalize (multiplyPoly x y)
+
+a = Mono [Var "x" 3] 3
+b = Mono [Var "y" 2] 6
+c = Mono [Var "z" 5] 2
+d = Mono [Var "x" 5] 9
+n = Mono [Var "y" 2] 5
+
+x0 = Var "x" 0
+x1 = Var "x" 1
+x2 = Var "x" 2
+x3 = Var "x" 3
+
+y0 = Var "y" 0 
+y1 = Var "y" 1
+y2 = Var "y" 2
+y3 = Var "y" 3
+
+z0 = Var "z" 0 
+z1 = Var "z" 1
+z2 = Var "z" 2
+z3 = Var "z" 3
